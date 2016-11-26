f926b003909250bc326dae96a26347d4b24ef037
wsycarlos <wsycarlos@126.com>
2016-11-26 16:29:27 -0330

    Init

---
+++ Assets/LeapNet/Scripts/LeapNet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace LeapNet
+{
+    public class LeapNet : NetworkManager
+    {
+        public bool isDebug = false;
+
+        void Start()
+        {
+            if (isDebug)
+            {
+                networkAddress = "127.0.0.1";
+                StartClient();
+            }
+            else
+            {
+                networkAddress = "192.168.137.184";
+                StartClient();
+            }
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            Debug.Log("Server Connected!");
+        }
+
+        public override void OnClientDisconnect(NetworkConnection conn)
+        {
+            Debug.Log("Server Disconnected!");
+        }
+    }
+}
